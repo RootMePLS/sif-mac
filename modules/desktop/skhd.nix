d4d1ea2a88bf1f9d211255539b318d82e8767e4c
Dmitrii Miroshnichenko <2543810@gmail.com>
2023-07-23 19:56:05 +0200

    test

--- modules/desktop/skhd.nix
+++ modules/desktop/skhd.nix
@@ -6,59 +6,55 @@
       enable = true;
       package = pkgs.skhd;
       skhdConfig = ''
-        # Open Terminal
-        alt - return : /Applications/Alacritty.App/Contents/MacOS/alacritty
+        # alt + a / u / o / s are blocked due to umlaute
 
-        # Toggle Window
-        lalt - t : yabai -m window --toggle float && yabai -m window --grid 4:4:1:1:2:2
-        lalt - f : yabai -m window --toggle zoom-fullscreen
-        lalt - q : yabai -m window --close
+        # workspaces
+        ctrl + alt - j : ${pkgs.yabai} -m space --focus prev
+        ctrl + alt - k : ${pkgs.yabai} -m space --focus next
+        cmd + alt - j : ${pkgs.yabai} -m space --focus prev
+        cmd + alt - k : ${pkgs.yabai} -m space --focus next
 
-        # Focus Window
-        lalt - up : yabai -m window --focus north
-        lalt - down : yabai -m window --focus south
-        lalt - left : yabai -m window --focus west
-        lalt - right : yabai -m window --focus east
+        # send window to space and follow focus
+        ctrl + alt - l : ${pkgs.yabai} -m window --space prev; ${pkgs.yabai} -m space --focus prev
+        ctrl + alt - h : ${pkgs.yabai} -m window --space next; ${pkgs.yabai} -m space --focus next
+        cmd + alt - l : ${pkgs.yabai} -m window --space prev; ${pkgs.yabai} -m space --focus prev
+        cmd + alt - h : ${pkgs.yabai} -m window --space next; ${pkgs.yabai} -m space --focus next
 
-        # Swap Window
-        shift + lalt - up : yabai -m window --swap north
-        shift + lalt - down : yabai -m window --swap south
-        shift + lalt - left : yabai -m window --swap west
-        shift + lalt - right : yabai -m window --swap east
+        # focus window
+        alt - h : ${pkgs.yabai} -m window --focus west
+        alt - l : ${pkgs.yabai} -m window --focus east
 
-        # Resize Window
-        shift + cmd - left : yabai -m window --resize left:-50:0 && yabai -m window --resize right:-50:0
-        shift + cmd - right : yabai -m window --resize left:50:0 && yabai -m window --resize right:50:0
-        shift + cmd - up : yabai -m window --resize up:-50:0 && yabai -m window --resize down:-50:0
-        shift + cmd - down : yabai -m window --resize up:-50:0 && yabai -m window --resize down:-50:0
+        # focus window in stacked
+        alt - j : if [ "$(${pkgs.yabai} -m query --spaces --space | jq -r '.type')" = "stack" ]; then ${pkgs.yabai} -m window --focus stack.next; else ${pkgs.yabai} -m window --focus south; fi
+        alt - k : if [ "$(${pkgs.yabai} -m query --spaces --space | jq -r '.type')" = "stack" ]; then ${pkgs.yabai} -m window --focus stack.prev; else ${pkgs.yabai} -m window --focus north; fi
 
-        # Focus Space
-        ctrl - 1 : yabai -m space --focus 1
-        ctrl - 2 : yabai -m space --focus 2
-        ctrl - 3 : yabai -m space --focus 3
-        ctrl - 4 : yabai -m space --focus 4
-        ctrl - 5 : yabai -m space --focus 5
-        #ctrl - left : yabai -m space --focus prev
-        #ctrl - right: yabai -m space --focus next
+        # swap managed window
+        shift + alt - h : ${pkgs.yabai} -m window --swap west
+        shift + alt - j : ${pkgs.yabai} -m window --swap south
+        shift + alt - k : ${pkgs.yabai} -m window --swap north
+        shift + alt - l : ${pkgs.yabai} -m window --swap east
 
-        # Send to Space
-        shift + ctrl - 1 : yabai -m window --space 1
-        shift + ctrl - 2 : yabai -m window --space 2
-        shift + ctrl - 3 : yabai -m window --space 3
-        shift + ctrl - 4 : yabai -m window --space 4
-        shift + ctrl - 5 : yabai -m window --space 5
-        shift + ctrl - left : yabai -m window --space prev && yabai -m space --focus prev
-        shift + ctrl - right : yabai -m window --space next && yabai -m space --focus next
+        # increase window size
+        shift + alt - a : ${pkgs.yabai} -m window --resize left:-20:0
+        shift + alt - s : ${pkgs.yabai} -m window --resize right:-20:0
 
-        # Menu
-        #cmd + space : for now its using the default keybinding to open Spotlight Search
+        # toggle layout
+        alt - t : ${pkgs.yabai} -m space --layout bsp
+        alt - d : ${pkgs.yabai} -m space --layout stack
+
+        # float / unfloat window and center on screen
+        alt - n : ${pkgs.yabai} -m window --toggle float; \
+                  ${pkgs.yabai} -m window --grid 4:4:1:1:2:2
+
+        # toggle sticky(+float), topmost, picture-in-picture
+        alt - p : ${pkgs.yabai} -m window --toggle sticky; \
+                  ${pkgs.yabai} -m window --toggle topmost; \
+                  ${pkgs.yabai} -m window --toggle pip
+
+        # reload
+        shift + alt - r : brew services restart skhd; brew services restart yabai; brew services restart sketchybar
       '';                                 # Hotkey config
     };
   };
 
-  system = {
-    keyboard = {
-      enableKeyMapping = true;            # Needed for skhd
-    };
-  };
 }
